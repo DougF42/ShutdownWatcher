fed08720e1bf90903e9d3e4248dd0f3b8fd0625c
DougOnPi <doug@fajardo.hm>
2023-05-10 00:23:33 -0400

    General cleanup and test

--- shutdownwatcher.cpp
+++ shutdownwatcher.cpp
@@ -21,168 +21,61 @@
 #include <string.h>
 #include <unistd.h>
 #include <wiringPi.h>
-#include <regex.h>
+#include <time.h>
 #include "parseDefaults.h"
+
 
 // IF defined, we wait for an interrupt.
 #define USE_INTERRUPT
-
+#define DEFAULT_FILE_NAME "/etc/defaults/shutdownwatcher"
 // IF defined, we dont actually reboot, but instead put out a stslog message.
 #define TESTMOD
 
-/**
- * Identifies wether we want UART or SPI driver
- */
-enum QCA_MODE {QCA_UART, QCA_SPI};
-typedef enum QCA_MODE QCA_MODE_t;
-
-
+#define UART_MODE 1
+#define SPI_MODE  0
 using namespace std;
 
+// Line 1 - UART or SPI only.
+#define RUN_FILE_NAME "/var/run/shutdownwatcher/status"
 
-/**
- * make sure the config lines are correct for the
- * chosen QCA7000 mode.
- * Specifically:
- * (1) enable_uart=0 or =1 as needed
- * (2) dtparam=spi=on or off
- * (3) uncomment one, comment the other of these:
- *  dtoverlay=qca7000 ...
- *  dtoverlay=qca7000-uart0...
- *
- * @param bfr point to null-terminated buffer
- * @param qca_mode do we want UART or SPI? 
+/** @brief
+ * returns 0 for SPI mode, 1 for UART mode, -1 if any error (not present?)
  */
-
-#define TARGET_SPI      "dtparam=spi="
-#define TARGET_QCA_SPI  "dtoverlay=qca7000,"
-#define TARGET_QCA_UART "dtoverlay=qca7000-uart0"
-
-
-#define HAVE_enable 1
-#define HAVE_spi    2
-#define HAVE_qca7000 4
-static u_int8_t found_statements=0;
-void update_line(char *bfr, QCA_MODE_t qca_mode)
+int checkRunState()
 {
-  // TODO
-  // determine line type, and take action
-  int pos = 0;
-  char wantedSetting[3];  // internal work item
-
-  #define TARGET_ENABLE   "enable_uart="
-  if (0==strncmp(TARGET_ENABLE, bfr+pos, strlen(TARGET_ENABLE)))
-    { // found the 'enable_uart statement
-      pos=strlen(TARGET_ENABLE)+1; // point to the 0 or 1
-      found_statements |= HAVE_enable; // mark that we found it
-      if  (qca_mode == QCA_UART)
-	wantedSetting[0]= '1';
-      else
-	wantedSetting[0] = '0';
-      
-      if (bufr[pos] == wantedSetting[0]) return(0); // already set
-      bufr[pos] = wantedSetting[0];
-      return;
-    }
-
-  // Find any DTPARM statement...
-  #define SEARCH "^[#]?dtoverlay=([a-z_]+)(.*)"
-  regex_t *expr;
-  size_t nmatch[5];
-  int res=regcomp( expr, SEARCH, REG_EXTENDED|REG_ICASE);
-  if (res!=0)
+  char buf[5];
+  FILE *file = fopen(RUN_FILE_NAME,"w");
+  if (file == NULL)
     {
-      
+      // TODO: error!;
     }
-  
-
-  
-  if (0==strncmp(TARGET_SPI, buf+pos))
-  { // dtparam=spi=on/off statement
-      pos=strlen(TARGET_ENABLE)+1;
-      found_statements |= HAVE_spi;
-      
-      if  (qca_mode == QCA_UART)
-	wantedSetting= '1';
-      else
-	wantedSetting = '0';
-      
-      if (bufr[pos] == wantedSetting) return(0); // already set
-      bufr[pos] = wantedSetting;
-    }
-  return(false);
+  size_t cnt=fread(buf, 1, 5, file);
+  fclose(file);
+  if (cnt !=5) return(-1);
+  buf[4] = '\0';
+  if (0 == strcmp(buf,"UART")) return(UART_MODE);
+  if (0 == strcmp(buf,"SPI.")) return(SPI_MODE);
+  return(-1);
 }
 
-
 /**
- * This copies the COINFIG_TXT file to CONFIG_TXT_WRK, 
- * using 'update_line' to make and needed modifications. 
- * @param qca_mode UART or SPI?
- * @return true if a change was made (will need to reboot)
+ * Writes the current state to the run file
+ * @param state 0 for SPI mode, 1 for UART mode.
  */
-bool copyAndUpdateFile(QCA_MODE qca_mode)
+void writeRunState(int  state)
 {
-  found_Statements=0;      // Initialize
-  bool madeChanges=false ; // Did we make any changes?
-  char buf[256];           // space for one line...
-  int readlen;             // How many chars did we read?
+  FILE *file = fopen(RUN_FILE_NAME,"w");
+  if (file == NULL) return; // do nothing on error
   
-  // OPEN CONFIG_TXT for read
-  FILE *infile=fopen(CONFIG_TXT, "r");
-  
-  if (!infile)
-    {     
-      perror(CONFIG_TXT);
-      exit(1);
-    }
-
-  // OPEN CONFIG_TXT_WRK for write
-  FILE *wrkfile=fopen(CONFIG_TXT_WRK, "w");
-  if (!wrkfile)
+  if (state == UART_MODE)
     {
-      fclose(infile);
-      perror(CONFIG_TXT_WRK);
-      exit(2);
-    }
-  
-  while(1)
-    { // read, edit or copy  1 line.
-      readlen = fread(buf, 1, sizeof(buf), infile);
-      if (ferror(infile))
-	{ // OOPS!
-	  fclose(infile);
-	  fclose(wrkfile);
-#ifdef DELETE_WRK_FILE
-	  remove(CONFIG_TXT_WRK);
-#endif
-	}
-	  
-      if (feof(infile))
-	{ // all done
-	  fclose(infile);
-	  fclose(wrkfile);
-	  break;
-	}
-
-      
-      madeChanges |= update_line(buf, qca_mode);
-      // write result to output
-      fwrite(buf, 1, strlen(buf), wrkfile);
-    }
-  fclose(infile);
-  fclose(wrkfile);
-  if (madeChanges)
-    {
-      int res=rename(CONFIG_TXT_WRK, CONFIG_TXT);
-      if (res!=0)
-	{
-	  perror("RENAME FAILED:");	  
-	}
-      
-    }
-  return(madeChanges);
+      fprintf(file,"UART\n");
+    }  else {
+    fprintf(file,"SPI.\n");  
+  }
+  fclose(file);
+  return;
 }
-
 
 
 /**
@@ -199,33 +92,64 @@ void callShutdown(void)
   exit(0);
 }
 
+
 /**
  * MAIN entry point.
  * IF present, the first argument is the configuration 
  * file (default: /etc/default/shutdownwatcher)
  */ 
 int main(int argc, char **argv) {
-  // TODO: GET DEFAULT CONFIG VALUES
-  
-  int switchno = SHUTDOWN_GPIO_PIN;
- 
-  QCA_MODE_t qcamode=QCA_SPI;  // an initial assumption
-  
   syslog(LOG_DAEMON|LOG_EMERG, "Power button watcher started");
-  wiringPiSetupGpio();   // Use broadcom GPIO pin numbers
-  pinMode(switchno, INPUT);  // set switch to input mode
-  pullUpDnControl (SHUTDOWN_GPIO_PIN, PUD_UP) ; // set Pull-up 50k resistor
-  syslog(LOG_DAEMON|LOG_EMERG, "GPIO configured");
-
-  pinMode(PICK_UART_GPIO_PIN, INPUT); // UART or SPI mode?
-  pullUpDnControl(SHUTDOWN_GPIO_PIN, PUD_UP); 
-  
-  // TODO: Configure and check for SPI vrs UART mode
-  if (digitalRead(PICK_UART_GPIO_PIN))
+  const char *fname=  DEFAULT_FILE_NAME ;
+  parseDefaults parser;
+  if (argc==2)
     {
-      copyAndUpdateFile(QCA_UART);
-    } else {
-      copyAndUpdateFile(QCA_SPI);
+      if ( 0 == strcmp(argv[2],"?"))
+	{
+	  help();
+	  return(0);
+	} else  {
+	fname = argv[1]; 
+      }
+    }
+  syslog(LOG_DAEMON|LOG_EMERG, "Parse file %s", fname);          
+  parser.begin(fname);
+  syslog(LOG_DAEMON|LOG_EMERG,"Parse complete. Values:");
+  syslog(LOG_DAEMON|LOG_EMERG,"MAIN CONFIG file: %s\n",parse.configtxt_main().c_str() );
+  syslog(LOG_DAEMON|LOG_EMERG,"UART CONFIG file: %s\n",parse.configtxt_uart().c_str() );
+  syslog(LOG_DAEMON|LOG_EMERG,"SPI  CONFIG file: %s\n",parse.configtxt_spi().c_str() );
+  syslog(LOG_DAEMON|LOG_EMERG," \n");
+  syslog(LOG_DAEMON|LOG_EMERG,"Shutdown pin: %d\n",   parse.shutdownPin() );
+  syslog(LOG_DAEMON|LOG_EMERG,"Uart pin: %d\n",       parse.uartPin() );
+  syslog(LOG_DAEMON|LOG_EMERG,"heart Beat pin: %d\n",  parse.heartbeatPin() );
+  syslog(LOG_DAEMON|LOG_EMERG,"heart Rate: %d\n",      parse.heartbeatRate() );	 
+  syslog(LOG_DAEMON|LOG_EMERG,"FINISHED\n\n");
+  
+
+  wiringPiSetupGpio();         // Use broadcom GPIO pin numbers
+  pinMode(parse.shutdownpin(), INPUT);   // shut down pin - input
+  pullUpDnControl (parse.shutdownpin(), PUD_UP) ; // set Pull-up 50k resistor
+  
+  pinMode(parse.hearbeatPin(), OUTPUT);   // LED PIN - output
+  pullUpDnControl (parse.shutdownpin(), PUD_UP) ; // set Pull-up 50k resistor
+  
+
+  pinMode(parse.uartPin(), INPUT); // UART or SPI mode pin - INPUT
+  pullUpDnControl(parse.uartPin(), PUD_UP); 
+  
+  // Check and configure UART or SPI mode if changed
+  int oldState = checkRunState();
+  int curState = digitalRead(parse.uartPin()) == 0; // true if UART
+  if ((oldState<0) || (oldState != curState)
+    {  // change the state 
+      if (curState)
+	{ // UART mode
+	  system("cp",parse.configtxt_uart().c_str(), parse.configtxt.main().c_str());
+	  writeRunState(parse.uartPin());
+	} else {
+	// SPI mode
+	system("cp", parse.configtxt_spi().c_str(),  parse.configtxt.main().c_str());
+	writeRunState(parse.uartPin());
     }
     // TODO: REBOOT IF ANY CHANGE?
 
